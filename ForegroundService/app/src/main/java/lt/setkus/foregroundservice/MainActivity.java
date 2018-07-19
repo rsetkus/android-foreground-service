2b4e98651c6c559493b64d148d5498f025644222
Robertas Setkus <robertas.setkus@gmail.com>
2018-07-19 22:23:25 +0100

    Through the Intent service expects to get a Notification object to use when the service is starting in foreground mode.

--- ForegroundService/app/src/main/java/lt/setkus/foregroundservice/MainActivity.java
+++ ForegroundService/app/src/main/java/lt/setkus/foregroundservice/MainActivity.java
@@ -1,13 +1,32 @@
 package lt.setkus.foregroundservice;
 
+import android.app.Notification;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String NOTIFICATION_CHANNEL_ID = "long_running_service_channel";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LongRunningService.class);
+        intent.putExtra(LongRunningService.NOTIFICATION, buildNotification());
+        startService(intent);
+    }
+
+    private Notification buildNotification() {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(getString(R.string.foreground_service_title))
+            .setContentText(getString(R.string.foreground_service_message))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        return notificationBuilder.build();
     }
 }
--- ForegroundService/foregroundservice/src/main/java/lt/setkus/foregroundservice/LongRunningService.java
+++ ForegroundService/foregroundservice/src/main/java/lt/setkus/foregroundservice/LongRunningService.java
@@ -1,15 +1,29 @@
 package lt.setkus.foregroundservice;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
 
 public class LongRunningService extends Service {
+
+    private static final int LONG_RUNNING_SERVICE_ID = 1;
+
+    public static String NOTIFICATION = "notification_icon";
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Notification notification = intent.getExtras().getParcelable(NOTIFICATION);
+        startForeground(LONG_RUNNING_SERVICE_ID, notification);
+        return START_STICKY;
     }
 }
